90e966c8c946199078101407272ab86ea57fdaab
Juan Pedro Ghm <juanpedroghm@gmail.com>
2020-03-22 17:29:13 +0100

    Moved Suggestion component logic to ChooseOption component, added databaseInit file

--- src/components/chooseOption/ChooseOption.jsx
+++ src/components/chooseOption/ChooseOption.jsx
@@ -1,17 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './chooseOption.css'
+import {db} from '../firebase/firebase'
 
-export default function ChooseOption(){
-    return (
-        <div className="ChooseOption">
-                <div className="guideText">
-                        choose
+export default class ChooseOption extends React.Component{
+
+
+    componentDidMount() {
+        this.randomSuggestion();
+    }
+    
+    render(){
+        if (this.state != undefined){
+            return (
+                <div className="ChooseOption">
+                        <div className="guideText">
+                        <p className="activityTitle">{this.state.suggestion.title}</p>
+                        <p className="activityDescription">{this.state.suggestion.description}</p>
+                        </div>
+                        <ul className="buttonContainer">
+                            <li><Link to="submit"><button className="button">contribute ideas</button></Link></li>
+                            <li><button className="button" onClick={() => this.randomSuggestion()}>new suggestion</button></li>
+                        </ul>
                 </div>
-                <ul className="buttonContainer">
-                    <li><Link to="submit"><button className="button">contribute ideas</button></Link></li>
-                    <li><button className="button">new suggestion</button></li>
-                </ul>
-        </div>
-    )
+            )    
+        }else{
+            return(
+                <div className="ChooseOption">
+                       
+                </div>
+            )
+        }   
+    }
+
+    randomSuggestion(){
+        if (this.state == undefined){
+            let activities = db.ref('actions/');
+            activities.on('value', function (snapshot) {
+                    let suggestionList = snapshot.val();
+                    console.log(suggestionList)
+                    this.setState({
+                        suggestionList: suggestionList,
+                        suggestion: suggestionList[Math.floor(Math.random() * suggestionList.length)]
+                    });
+            }.bind(this));
+        }else{
+            this.setState({
+                suggestion : this.state.suggestionList[Math.floor(Math.random() * this.state.suggestionList.length)]
+            })
+        }
+    }
+        
 }
