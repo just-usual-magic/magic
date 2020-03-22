047aab612ce31e1e8784235323d04a7037c3bc69
Anna <asinitsyna@inovex.de>
2020-03-22 14:47:18 +0100

    Suggestion component now actually takes a random suggestion, and just makes one request on creation, not 2

--- src/components/suggestion/Suggestion.jsx
+++ src/components/suggestion/Suggestion.jsx
@@ -5,41 +5,47 @@ import {db} from '../firebase/firebase'
 export default class Suggestion extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {
-            suggestion : {
-                Title : "empty"
-            },
-        }
-        this.randomSuggestion()
     }
 
-    componentDidMount(){
-n
+    componentDidMount() {
+        this.randomSuggestion();
     }
-
 
     render() {
-        console.log("Rendering")
         console.log(this.state)
-        return(
-            <div className="Suggestion">
-                <div className="guideText">
-                    {this.state.suggestion.Title}
+        if(this.state != undefined){
+            return(
+                <div className="Suggestion">
+                    <div className="guideText">
+                        {this.state.suggestion.title}
+                    </div>
+                    <ul className="buttonContainer">
+                        <li><Link to="/"><button className="button">To beginning</button></Link></li>
+                    </ul>
                 </div>
-                <ul className="buttonContainer">
-                    <li><Link to="/"><button className="button">To beginning</button></Link></li>
-                </ul>
-            </div>
-        )
+            )
+        }
+        else{
+            return(
+                <div className="Suggestion">
+                    <div className="guideText">
+
+                    </div>
+                    <ul className="buttonContainer">
+                        <li><Link to="/"><button className="button">To beginning</button></Link></li>
+                    </ul>
+                </div>
+            )
+        }
     }
 
     randomSuggestion(){
-        let activities = db.ref('activities/');
-        activities.on('value', function(snapshot){
+        let activities = db.ref('actions/');
+        activities.on('value', function (snapshot) {
             let suggestionList = snapshot.val();
-            console.log(suggestionList);
+            console.log(suggestionList)
             this.setState({
-                suggestion : suggestionList["10pushups"]
+                suggestion: suggestionList[Math.floor(Math.random() * suggestionList.length)]
             });
         }.bind(this));
     }
